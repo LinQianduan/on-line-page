8ee3576a4a09dc773a398330bc64497c1fd1632a
林盼 <10482849+linpan11@user.noreply.gitee.com>
2023-02-09 10:11:29 +0800

    初始化page分支

--- src/routers/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import { staticRouter, errorRouter, notFoundRouter } from "@/routers/modules/staticRouter";
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [...staticRouter, ...errorRouter, notFoundRouter ]
-})
-
-export default router
